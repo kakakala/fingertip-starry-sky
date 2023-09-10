add4843c58eb09b7e984763d4359c9a1b1499277
kakakala <8319gary@gmail.com>
2023-09-10 23:36:55 +0800

    index初步完成

--- main.js
+++ main.js
@@ -2,3 +2,21 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+      delay: 5000,
+    },
+     breakpoints: {
+       768: {
+         autoplay: {
+           enabled: false,
+         }
+       }
+     },
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+ });
+ 
