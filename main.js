58c3c1905e2fc5e0cd090f5420c8d39972b2706c
希月 <designmia8@gmail.com>
2023-09-15 21:30:49 +0800

    add service

---
+++ main.js
@@ -0,0 +1,28 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+console.log("Hello world!");
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  loop: true,
+    autoplay: { 
+      delay: 5000,
+    },
+     breakpoints: {
+       768: {
+         autoplay: {
+           enabled: false,
+         }
+       }
+     },
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+   pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+ });
+ 
